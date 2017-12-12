aee6b7d59060f8c70fa3ba91195764065e027e35
spookybird <wpoqwpoqw@gmail.com>
2017-12-12 10:38:24 +0900

    Update applicationbox.js

--- core/js/applicationbox.js
+++ core/js/applicationbox.js
@@ -114,7 +114,7 @@
       '  width: unset;',
       '}',
       '#imui-container div.imbox div.imbox-timeline div.imbox-timeline-body #imbox_timeline_thread div.imbox-timeline-thread-body span.imbox-timeline-thread-post-right div.imbox-timeline-thread-post-right-body div.imbox-timeline-thread-message div.imbox-timeline-thread-share span.imbox-timeline-thread-share-right span.imbox-timeline-thread-share-url {',
-      '  word-wrap: break-word;',
+      '  word-break: break-all;',
       '}'
     ].join('\n');
 
